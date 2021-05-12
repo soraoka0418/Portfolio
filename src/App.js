85e20b255aaea6642bb8dfad804d32867716eb86
okadasorachi <Soraoka0418@outlook.jp>
2021-05-12 22:34:39 +0900

    書き上げた

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/router";
-
+import "./index.css";
+import "./App.css";
+import { Header } from "./components/organisms/layout/Header";
 
 export default function App() {
   return (
     <ChakraProvider>
-       <BrowserRouter>
-          <Router/>
-       </BrowserRouter>
+      <BrowserRouter>
+        <Header/>
+        <Router  />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
--- src/components/organisms/layout/Header.js
+++ src/components/organisms/layout/Header.js
@@ -1,25 +1,91 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/layout"
-
+import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { Link } from "react-router-dom";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button, IconButton } from "@chakra-ui/button";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { useDisclosure } from "@chakra-ui/hooks";
 export const Header = () => {
-    return(
-        <Flex as = "nav" bg = "teal.500" color= "gray.50" align ="center" justify= "space-between" padding ={{base:3,md:5} }>
-            <Heading as = "h1" fontSize={{base:"md" , md:"lg"}} pr = {5}>Sorachi-portfolio</Heading>
+  const LinkStyle = {
+    fontSize: "32px",
+  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box
+      as="div"
+      bg="teal.500"
+      color="gray.50"
+      padding="8"
+      textAlign="center"
+      display={{ base: "flex", md: "block" }}
+      w="100%"
+      h={{ base: "20vh", md: "30vh" }}
+    >
+      <Heading as="h1" fontSize={{ base: "x-large", md: "xxx-large" }}>
+        Welcome to My Portfolio !
+      </Heading>
+      <Flex
+        as="nav"
+        justify="center"
+        padding={{ base: 3, md: 5 }}
+        mt={{ base: "3", md: "4" }}
+        fontSize="2xl"
+        display={{ base: "none", md: "flex" }}
+        _hover={{ cursor: "pointer" }}
+      >
+        <Box pl={8}>
+          <Link to="/" style={LinkStyle}>
+            home
+          </Link>
+        </Box>
 
-            <Flex align= "center" flexGrow={2} fontSize="sm">
-                <Box pr = {4} >
-                    <Link>home</Link>
-                </Box>
-                <Box pr = {4}>
-                    <Link>About</Link>
-                </Box>
-                <Box pr = {4}>
-                    <Link>Skills</Link>
-                </Box>
-                <Box pr = {4}>
-                    <Link>Contact</Link>
-                </Box>
-            </Flex>
-        </Flex>
-
-    )
-}
+        <Box pl={8}>
+          <Link to="/skill" style={LinkStyle}>
+            Skills
+          </Link>
+        </Box>
+        <Box pl={8}>
+          <Link to="/Contact" style={LinkStyle}>
+            Contact
+          </Link>
+        </Box>
+      </Flex>
+      <IconButton
+        ml="auto"
+        area-aria-label="メニューボタン"
+        icon={<HamburgerIcon />}
+        size="md"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+        onClick={onOpen}
+      />
+      <Drawer placement="left" isOpen={isOpen} onClose={onClose} size="xs">
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg={"gray.100"}>
+              <Link to="/">
+                <Button p="16" w="100%">
+                  Home
+                </Button>
+              </Link>
+              <Link to="/skill">
+                <Button p="16" w="100%">
+                  Skills
+                </Button>
+              </Link>
+              <Link to="/Contact">
+                <Button p="16" w="100%">
+                  Contact
+                </Button>
+              </Link>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Box>
+  );
+};
